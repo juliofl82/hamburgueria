2238def6bcb2136b06c2160e7af54a2b780813e0
juliofl82 <jfigueiredolamounier@gmail.com>
2024-04-24 10:58:24 -0300

    update modal

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -8,29 +8,29 @@ export const CartModal = ({ cartList, toggleCartVisibility }) => {
    }, 0);
 
    return (
-      
-      
 
-      <div className={styles.modalCard} role="dialog">
-         <div className={styles.modalHeader}>
-            <h2>Carrinho de compras</h2>
-            <button onClick={toggleCartVisibility} aria-label="close" title="Fechar">
-               <MdClose size={21} />
-            </button>
-         </div>
-         <div>
-            <ul>
-               {cartList.map((product) => (
-                  <CartItemCard key={product.id} product={product} />
-               ))}
-            </ul>
-         </div>
-         <div>
-            <div>
-               <span>Total</span>
-               <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
+      <div className={styles.modalOverlay}>
+         <div className={styles.modalCard} role="dialog">
+            <div className={styles.modalHeader}>
+               <h2>Carrinho de compras</h2>
+               <button onClick={toggleCartVisibility} aria-label="close" title="Fechar">
+                  <MdClose size={21} />
+               </button>
             </div>
-            <button>Remover todos</button>
+            <div>
+               <ul>
+                  {cartList.map((product) => (
+                     <CartItemCard key={product.id} product={product} />
+                  ))}
+               </ul>
+            </div>
+            <div>
+               <div>
+                  <span>Total</span>
+                  <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</span>
+               </div>
+               <button>Remover todos</button>
+            </div>
          </div>
       </div>
    );
