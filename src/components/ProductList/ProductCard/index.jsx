e03e8523f833a253439e34c17fde27ccd752cccf
juliofl82 <jfigueiredolamounier@gmail.com>
2024-04-26 01:48:59 -0300

    update responsivo

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -2,17 +2,21 @@ import styles from "./styles.module.scss";
 
 export const ProductCard = ({ product, addToCart }) => {
     return(
-        <li className={styles.cardGrid}>
+        
+        <div className={styles.cardContainer} >
+           <li className={styles.cardGrid}>
             <div className={styles.imgBox}>
                 <img src={product.img} alt={product.name} />
                 </div>            
-            
             <div className={styles.itensInfo}>
                 <h3 className={styles.productName}>{product.name}</h3>
                 <span className={styles.productCategory}>{product.category}</span>
                 <span className={styles.price} >{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
                 <button onClick={() => addToCart(product)}>Adicionar</button>
             </div>
-        </li>
+        </li> 
+        </div>
+
+        
     )
 }
